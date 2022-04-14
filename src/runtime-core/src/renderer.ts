59b25c224d66cda6a62e707231ed589df1f4b48b
zhouqi <zq@differsoft.com>
2022-04-14 10:54:59 +0800

    fix：快速diff bug修复

--- src/runtime-core/src/renderer.ts
+++ src/runtime-core/src/renderer.ts
@@ -2,7 +2,7 @@
  * @Author: Zhouqi
  * @Date: 2022-03-26 21:59:49
  * @LastEditors: Zhouqi
- * @LastEditTime: 2022-04-14 10:44:53
+ * @LastEditTime: 2022-04-14 10:54:33
  */
 import { createComponentInstance, setupComponent } from "./component";
 import { Fragment, isSameVNodeType, Text, Comment } from "./vnode";
