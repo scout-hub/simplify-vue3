3d1b12defda6a5040e26f0cf16b1409f219d7803
zhouqi <zq@differsoft.com>
2022-04-21 22:53:31 +0800

    refactor：修改example

--- example/compileTemplate/App.js
+++ example/compileTemplate/App.js
@@ -2,17 +2,17 @@
  * @Author: Zhouqi
  * @Date: 2022-03-26 21:17:12
  * @LastEditors: Zhouqi
- * @LastEditTime: 2022-04-21 22:40:08
+ * @LastEditTime: 2022-04-21 22:52:37
  */
 import {
     ref
 } from "../../lib/simplify-vue.esm.js";
 export default {
-    template: `<div>{{ msg }}<p>123</p>{{ msg1 }}<h1>哈哈哈，{{ msg }}</h1></div>`,
+    template: `<div>{{ msg }}<h1>{{ msg1 }}</h1><p>hi，<span>scout</span></p></div>`,
     name: "App",
     setup() {
-        const msg = ref('msg');
-        const msg1 = ref('msg1');
+        const msg = ref('hello');
+        const msg1 = ref('simplify-vue');
         return {
             msg,
             msg1
